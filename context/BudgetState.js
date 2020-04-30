d9a1e96f360ac6122464847741d64645182e980f
Trevor Debardeleben <trevordebardeleben@Trevors-MacBook-Pro.local>
2020-04-29 21:40:52 -0500

    Populate initial budget state from api

--- context/BudgetState.js
+++ context/BudgetState.js
@@ -1,37 +1,11 @@
 import React, { useReducer } from 'react';
 import budgetReducer from './budgetReducer';
-import { sumBudget } from '../lib/budgetUtils';
 
-const budget = [
-  { category: 'Eating Out', value: 200 },
-  { category: 'Bars', value: 100 },
-  { category: 'Groceries', value: 250 },
-];
-
-const transactions = [
-  { label: 'Podunk Bar LLC', category: 'Bars', amount: 30 },
-  { label: `Restaurants R Us`, category: 'Eating Out', amount: 50 },
-  { label: 'Walmart', category: 'Groceries', amount: 120 },
-  { label: `Converted`, amount: 1000 },
-  { label: `Ignore This Category`, category: 'ignore', amount: 3000 },
-];
-const initalState = {
-  total: 2500,
-  budget,
-  toBeBudgeted: 2500 - budget.reduce(sumBudget, 0),
-  transactions,
-};
-/**
- * TODO: ast minute thoughts
- * - Transactions should just have the ability to have not category. this is useful for importing your past transactions when you set up an acount so that
- * they do not impact your buckets.
- * - or "No Impact" could be a category for all imported transactions so that they don't impact budget
- * - or maybe just don't import old transactions ?? Leaning towards this...Need to revisit
- */
 export const BudgetContext = React.createContext({ state: null, dispatch: null });
 
 const BudgetState = props => {
-  const [accountState, dispatch] = useReducer(budgetReducer, initalState);
+  const { initialState } = props;
+  const [accountState, dispatch] = useReducer(budgetReducer, initialState[0]);
   return <BudgetContext.Provider value={{ dispatch, state: accountState }}>{props.children}</BudgetContext.Provider>;
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import fetch from 'isomorphic-fetch';
 import Head from '../components/head';
 import Budget from '../components/Budget';
 import Transactions from '../components/Transactions';
 import BudgetState from '../context/BudgetState';
 
-const Home = () => (
-  <BudgetState>
-    <Head title="Budget Trace" />
-    <h1>Budget Trace</h1>
-    <Budget />
-    <Transactions />
-  </BudgetState>
-);
-
+const Home = props => {
+  const { data: budgetData } = props;
+  return (
+    <BudgetState initialState={budgetData}>
+      <Head title="Budget Trace" />
+      <h1>Budget Trace</h1>
+      <Budget />
+      <Transactions />
+    </BudgetState>
+  );
+};
+export async function getServerSideProps() {
+  const res = await fetch('http://localhost:3000/api/budget');
+  const data = await res.json();
+  return {
+    props: data,
+  };
+}
 export default Home;
