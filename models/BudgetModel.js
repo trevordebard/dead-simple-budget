e649048d65752e31d3f09b9252e7b428013f81eb
Trevor Debardeleben <trevordebardeleben@Trevors-MacBook-Pro.local>
2020-04-30 20:55:01 -0500

    Make fields required is BudgetSchema

--- models/BudgetModel.js
+++ models/BudgetModel.js
@@ -1,9 +1,20 @@
 import mongoose from 'mongoose';
 
+const TransactionSchema = new mongoose.Schema({
+  description: { type: String, required: true },
+  stack: { type: String, required: true },
+  date: Date,
+  amount: { type: Number, required: true },
+});
+
 const BudgetSchema = new mongoose.Schema({
-  total: Number,
-  toBeBudgeted: Number,
-  stack: [{ label: String, value: Number }],
-  transactions: [{ description: String, stack: String, date: Date, amount: Number }],
+  total: { type: Number, required: true },
+  toBeBudgeted: { type: Number, required: true },
+  stacks: [{ label: { type: String, required: true }, value: { type: Number, required: true } }],
+  transactions: {
+    required: true,
+    validate: [value => value.length > 0, 'Transactions array cannot be empty'],
+    type: [TransactionSchema],
+  },
 });
 export default mongoose.models.Budget || mongoose.model('Budget', BudgetSchema);
