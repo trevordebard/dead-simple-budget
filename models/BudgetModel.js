c9e10c985aac72e698dc986cb3297855849504fe
Trevor Debardeleben <trevordebardeleben@Trevors-MacBook-Pro.local>
2020-04-30 19:04:32 -0500

    Update budget schema to use stack naming

--- models/BudgetModel.js
+++ models/BudgetModel.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const BudgetSchema = new mongoose.Schema({
   total: Number,
   toBeBudgeted: Number,
-  budget: [{ category: String, value: Number }],
-  transactions: [{ label: String, category: String, date: Date, amount: Number }],
+  stack: [{ label: String, value: Number }],
+  transactions: [{ description: String, stack: String, date: Date, amount: Number }],
 });
 export default mongoose.models.Budget || mongoose.model('Budget', BudgetSchema);
