74a72c4c48373fa80b506283a5fd2a3c2dc7f418
Trevor Debardeleben <trevordebardeleben@Trevors-MacBook-Pro.local>
2020-05-12 20:51:38 -0500

    Setup Apollo Client

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import fetch from 'isomorphic-fetch';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
 import Head from '../components/head';
 import Budget from '../components/Budget';
 import Transactions from '../components/Transactions';
 import BudgetState from '../context/BudgetState';
+import { withApollo } from '../lib/withApollo';
+
+const UserQuery = gql`
+  query user {
+    userById(_id: "5eb34a25f56c9892e1923fad") {
+      email
+      transactions {
+        description
+        amount
+      }
+      budget {
+        total
+        toBeBudgeted
+        stacks {
+          label
+          value
+        }
+      }
+    }
+  }
+`;
 
 const Home = props => {
-  const { data: budgetData } = props;
-  return (
-    <BudgetState initialState={budgetData}>
-      <Head title="Budget Trace" />
-      <h1>Budget Trace</h1>
-      <Budget />
-      <Transactions />
-    </BudgetState>
-  );
+  const data = useQuery(UserQuery);
+  if (!data.loading) {
+    const budgetData = data.data.userById;
+    return (
+      <BudgetState initialState={budgetData}>
+        <Head title="Budget Trace" />
+        <h1>Budget Trace</h1>
+        <Budget />
+        <Transactions />
+      </BudgetState>
+    );
+  }
+  return <p>Loading...</p>;
 };
-export async function getServerSideProps() {
-  const res = await fetch('http://localhost:3000/api/user');
-  const data = await res.json();
-  return {
-    props: data,
-  };
-}
-export default Home;
+export default withApollo({ ssr: true })(Home);
