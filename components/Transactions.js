f1ed1ce5f71ee3851154da82f979c7839ec25cdb
Trevor Debardeleben <trevordebardeleben@Trevors-MacBook-Pro.local>
2020-05-19 20:19:41 -0500

    Add required fields to transaction form

--- components/Transactions.js
+++ components/Transactions.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useMutation } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
-import { useForm } from 'react-hook-form';
+import { useForm, ErrorMessage } from 'react-hook-form';
 import useBudget from '../hooks/useBudget';
 
 const ADD_TRANSACTION = gql`
@@ -33,28 +33,43 @@ const Transactions = () => {
 };
 
 const NewTransaction = () => {
-  const { register, handleSubmit, watch, errors } = useForm();
+  const { register, handleSubmit, errors, reset } = useForm();
   const { state } = useBudget();
   const [addTransaction] = useMutation(ADD_TRANSACTION);
-
   const onSubmit = data => {
     const { description, amount, stack } = data;
     addTransaction({
       variables: { record: { description, amount: parseFloat(amount), stack, _userId: state._userId } },
     });
+    reset();
   };
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
-        Description: <input name="description" defaultValue="" ref={register} />
-        Amount: <input name="amount" ref={register} />
-        Date: <input name="date" ref={register} />
-        <select name="stack" ref={register}>
+        <label htmlFor="description">
+          Description: <input name="description" defaultValue="" ref={register({ required: 'Required' })} />
+          <ErrorMessage errors={errors} name="description">
+            {({ message }) => <span style={{ color: 'red' }}>{message} </span>}
+          </ErrorMessage>
+        </label>
+        <label htmlFor="amount">
+          Amount: <input name="amount" ref={register({ required: 'Required' })} />
+          <ErrorMessage errors={errors} name="amount">
+            {({ message }) => <span style={{ color: 'red' }}>{message} </span>}
+          </ErrorMessage>
+        </label>
+        <label htmlFor="date">
+          Date: <input name="date" ref={register} />
+        </label>
+        <select name="stack" ref={register({ required: true })}>
           {state.stacks.map(stackEl => (
             <option key={stackEl._id || `${stackEl.label}-${Date.now()}`} value={stackEl.label}>
               {stackEl.label}
             </option>
           ))}
+          <ErrorMessage errors={errors} name="stack">
+            {({ message }) => <span style={{ color: 'red' }}>{message} </span>}
+          </ErrorMessage>
         </select>
         <input type="submit" />
       </form>
