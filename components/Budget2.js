14905e60f494db8f7bc69b4256f880a4ff1308fa
Trevor Debardeleben <trevordebardeleben@Trevors-MacBook-Pro.local>
2020-05-31 16:37:08 -0500

    Update stack value onBlur

--- components/Budget2.js
+++ components/Budget2.js
@@ -1,50 +1,89 @@
 import React from 'react';
 import { useForm, ErrorMessage } from 'react-hook-form';
-import BudgetCategory from './BudgetCategory';
+import { useMutation, gql } from '@apollo/client';
 import useBudget2 from '../hooks/useBudget2';
 
+const UPDATE_STACK = gql`
+  mutation($budgetId: MongoID!, $label: String!, $value: Float!) {
+    budgetUpdateStack(budgetId: $budgetId, label: $label, value: $value) {
+      total
+      toBeBudgeted
+      stacks {
+        value
+        label
+      }
+    }
+  }
+`;
 function Budget2() {
-  const { data } = useBudget2();
-  const { register, handleSubmit, errors, reset } = useForm();
-  console.log(data);
+  const { data, loading } = useBudget2();
+  const { register, handleSubmit, errors } = useForm();
+  const [updateStack] = useMutation(UPDATE_STACK);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  const onSubmit = payload => {
+    console.log(payload);
+  };
   return (
     <div>
       <p>Budget2!</p>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="total">
-          Total: <input name="total" defaultValue="" ref={register} />
+          Total: <input name="total" defaultValue={data?.total} ref={register} />
           <ErrorMessage errors={errors} name="total">
             {({ message }) => <span style={{ color: 'red' }}>{message} </span>}
           </ErrorMessage>
         </label>
         <p style={{ color: 'red' }}>To Be Budgeted: {data?.toBeBudgeted}</p>
-        {renderStacks(data?.stacks)}
+        {renderStacks(data?.stacks, data._id)}
       </form>
       <hr></hr>
-      {/* <button
-          type="button"
-          onClick={() => {
-            addStack(newCategory);
-            setNewCategory('');
-          }}
-        >
-          Add Category
-        </button> */}
       <br />
     </div>
   );
-  function renderStacks(stacks) {
+  function renderStacks(stacks, budgetId) {
     return stacks?.map(item => (
-      <div>
-        <label htmlFor={item.label}>
-          {item.label}: <input name="total" defaultValue={item.value} ref={register} />
-          <ErrorMessage errors={errors} name="total">
-            {({ message }) => <span style={{ color: 'red' }}>{message} </span>}
-          </ErrorMessage>
-        </label>
+      <div key={item._id}>
+        <BudgetStack
+          label={item.label}
+          register={register}
+          budgetId={budgetId}
+          value={item.value}
+          errors={errors}
+          updateStack={updateStack}
+        />
       </div>
     ));
   }
 }
+
+const BudgetStack = ({ label, register, budgetId, value, errors, updateStack }) => (
+  <>
+    <label htmlFor={label}>
+      {label}:{' '}
+      <input
+        name={label}
+        onChange={e => console.log(e.target.value)}
+        onFocus={e => console.log(e.target.value)}
+        type="number"
+        onBlur={e => {
+          updateStack({
+            variables: {
+              budgetId,
+              label,
+              value: parseFloat(e.target.value),
+            },
+          });
+        }}
+        defaultValue={value}
+        ref={register}
+      />
+      <ErrorMessage errors={errors} name="total">
+        {({ message }) => <span style={{ color: 'red' }}>{message} </span>}
+      </ErrorMessage>
+    </label>
+  </>
+);
 
 export default Budget2;
