e37508392475a24f707cb0ab8e8ef37ac934c58a
Trevor Debardeleben <trevordebardeleben@Trevors-MacBook-Pro.local>
2020-05-31 20:37:24 -0500

    Refactor budget component to use Apollo Client

--- components/Budget.js
+++ components/Budget.js
@@ -1,33 +1,152 @@
 import React from 'react';
-import useInput from '../hooks/useInput';
-import BudgetCategory from './BudgetCategory';
-import useBudget from '../hooks/useBudget';
+import { useForm, ErrorMessage } from 'react-hook-form';
+import { useMutation, gql, resetApolloContext } from '@apollo/client';
+import useBudget2 from '../hooks/useBudget2';
 
-const Budget = () => {
-  const { state, addStack, updateAccountBalance } = useBudget();
-  const { total, toBeBudgeted, stacks } = state;
-  const [newCategory, onChangeNewCategory, setNewCategory] = useInput('');
+const UPDATE_STACK = gql`
+  mutation($budgetId: MongoID!, $label: String!, $value: Float!) {
+    budgetUpdateStack(budgetId: $budgetId, label: $label, value: $value) {
+      total
+      toBeBudgeted
+      stacks {
+        value
+        label
+      }
+    }
+  }
+`;
+const ADD_STACK = gql`
+  mutation($budgetId: MongoID!, $newStackLabel: String!, $newStackValue: Float) {
+    budgetPushToStacks(budgetId: $budgetId, newStackLabel: $newStackLabel, newStackValue: $newStackValue) {
+      total
+      toBeBudgeted
+      stacks {
+        label
+        value
+      }
+    }
+  }
+`;
+const REMOVE_STACK = gql`
+  mutation($budgetId: MongoID!, $label: String!) {
+    budgetRemoveStack(budgetId: $budgetId, label: $label) {
+      total
+      toBeBudgeted
+      stacks {
+        label
+        value
+      }
+    }
+  }
+`;
+function Budget2() {
+  const { data, loading } = useBudget2();
+  const { register, handleSubmit, errors, reset } = useForm();
+  const [addStack] = useMutation(ADD_STACK);
+  const [updateStack] = useMutation(UPDATE_STACK);
+  const [removeStack] = useMutation(REMOVE_STACK);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  const onSubmit = payload => {
+    console.log(payload);
+  };
   return (
     <div>
-      Total: <input name="total" type="number" value={total} onChange={e => updateAccountBalance(e.target.value)} />
-      <p style={{ color: 'red' }}>To Be Budgeted: {toBeBudgeted}</p>
-      {renderCategories(stacks)}
-      New Category: <input name="newCategory" type="text" value={newCategory} onChange={onChangeNewCategory} />
-      <button
-        type="button"
-        onClick={() => {
-          addStack(newCategory);
-          setNewCategory('');
-        }}
-      >
-        Add Category
-      </button>
+      <p>Budget2!</p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="total">
+            Total: <input name="total" defaultValue={data?.total} ref={register} />
+            <ErrorMessage errors={errors} name="total">
+              {({ message }) => <span style={{ color: 'red' }}>{message} </span>}
+            </ErrorMessage>
+          </label>
+        </div>
+        <div>
+          <p style={{ color: 'red' }}>To Be Budgeted: {data?.toBeBudgeted}</p>
+        </div>
+        {renderStacks(data?.stacks, data._id)}
+        <div style={{ marginTop: '10px' }}>
+          <label htmlFor="newStack">
+            New Stack: <input name="newStack" ref={register} />
+            <ErrorMessage errors={errors} name="total">
+              {({ message }) => <span style={{ color: 'red' }}>{message} </span>}
+            </ErrorMessage>
+            <button
+              type="button"
+              onClick={handleSubmit(payload => {
+                reset({ newStack: '' });
+                addStack({
+                  variables: { newStackLabel: payload.newStack, budgetId: data._id },
+                  refetchQueries: ['GET_BUDGET'], // Eventually change to update cache
+                });
+              })}
+            >
+              Add Stack
+            </button>
+          </label>
+        </div>
+      </form>
       <br />
     </div>
   );
-  function renderCategories(stacks) {
-    return stacks.map(item => <BudgetCategory key={`${item.label}}`} value={item.value} label={item.label} />);
+  function renderStacks(stacks, budgetId) {
+    return stacks?.map(item => (
+      <div key={item._id}>
+        <BudgetStack
+          label={item.label}
+          register={register}
+          budgetId={budgetId}
+          value={item.value}
+          errors={errors}
+          updateStack={updateStack}
+          removeStack={removeStack}
+        />
+      </div>
+    ));
   }
-};
+}
 
-export default Budget;
+const BudgetStack = ({ label, register, budgetId, value, errors, updateStack, removeStack }) => (
+  <>
+    <label htmlFor={label}>
+      {label}:{' '}
+      <input
+        name={label}
+        onChange={e => console.log(e.target.value)}
+        onFocus={e => console.log(e.target.value)}
+        type="number"
+        onBlur={e => {
+          updateStack({
+            variables: {
+              budgetId,
+              label,
+              value: parseFloat(e.target.value),
+            },
+          });
+        }}
+        defaultValue={value}
+        ref={register}
+      />
+      <button
+        type="button"
+        onClick={e => {
+          removeStack({
+            variables: {
+              budgetId,
+              label,
+            },
+          });
+        }}
+      >
+        Delete
+      </button>
+      <ErrorMessage errors={errors} name="total">
+        {({ message }) => <span style={{ color: 'red' }}>{message} </span>}
+      </ErrorMessage>
+    </label>
+  </>
+);
+
+export default Budget2;
--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,15 @@ import React from 'react';
 import Head from '../components/head';
 import Budget from '../components/Budget';
 import Transactions from '../components/Transactions';
-import BudgetState from '../context/BudgetState';
 import { withApollo } from '../lib/withApollo';
-import useBudget2 from '../hooks/useBudget2';
 
-const Home = () => {
-  // const data = useQuery(GET_USER);
-  const { data, loading } = useBudget2();
-  console.log(data);
-  if (!loading) {
-    // const budgetData = data.data.userById;
-    const budgetData = data.me.budget;
-    console.log(budgetData);
-    return (
-      <BudgetState initialState={budgetData}>
-        <Head title="Budget Trace" />
-        <h1>Budget Trace</h1>
-        <Budget />
-        <Transactions />
-      </BudgetState>
-    );
-  }
-  return <p>Loading...</p>;
-};
+const Home = () => (
+  <>
+    <Head title="Budget Trace" />
+    <h1>Budget Trace</h1>
+    <Budget />
+    <hr />
+    <Transactions />
+  </>
+);
 export default withApollo(Home);
