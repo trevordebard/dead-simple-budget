45a80203449ced89da59f28234b0b94e7484cc27
Trevor Debardeleben <trevordebardeleben@Trevors-MacBook-Pro.local>
2020-06-10 21:25:49 -0500

    Remove hardcoded user id

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import Nav from '../components/nav';
 function MyApp({ Component, pageProps, apollo }) {
   return (
     <>
-      <ApolloProvider client={apollo} userId="5ec1d97edd768b5259f24b50">
+      <ApolloProvider client={apollo}>
         <Nav />
         <Component {...pageProps} />
       </ApolloProvider>
