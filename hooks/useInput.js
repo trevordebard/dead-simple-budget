28d00d1a7620c223290da3f221c8a31406c36be2
Trevor Debardeleben <trevordebardeleben@Trevors-MacBook-Pro.local>
2020-06-10 21:42:30 -0500

    Remove unused files

--- hooks/useInput.js
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react';
-
-export default init => {
-  const [input, setInput] = useState(init);
-  const handleInputChange = e => setInput(e.currentTarget.value);
-  return [input, handleInputChange, setInput];
-};
