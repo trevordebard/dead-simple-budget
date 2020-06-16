aebe594b0a6f3898ff607c9a2cad54dd9d100ddb
Trevor Debardeleben <trevordebardeleben@Trevors-MacBook-Pro.local>
2020-06-15 22:01:46 -0500

    Add global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,29 @@
 import { ApolloProvider } from '@apollo/client';
+import styled, { createGlobalStyle } from 'styled-components';
 import { withApollo } from '../lib/withApollo';
 import Nav from '../components/nav';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`;
+const Content = styled.div`
+  width: 100%;
+  max-width: 682px;
+  margin: 0px auto;
+  padding: 0px 1rem;
+`;
 
 function MyApp({ Component, pageProps, apollo }) {
   return (
     <>
+      <GlobalStyle />
       <ApolloProvider client={apollo}>
         <Nav />
-        <Component {...pageProps} />
+        <Content>
+          <Component {...pageProps} />
+        </Content>
       </ApolloProvider>
     </>
   );
