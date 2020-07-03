5fd396901a3f2586518fe62a87e290f818ec6274
Trevor Debardeleben <trevordebardeleben@Trevors-MacBook-Pro.local>
2020-07-02 20:59:31 -0500

    Use Input styling with select list

--- components/FormInput.js
+++ components/FormInput.js
@@ -14,9 +14,9 @@ const FormInput = ({ register, name, errors, required = false, ...rest }) => (
 
 const FormSelect = ({ children, register, name, errors, required = false, ...rest }) => (
   <>
-    <select name={name} ref={register({ required: required && 'Required' })} {...rest}>
+    <Input as="select" name={name} ref={register({ required: required && 'Required' })} {...rest}>
       {children}
-    </select>
+    </Input>
     {errors && errors[name] && (
       <ErrorMessage errors={errors} name={name}>
         {({ message }) => <p style={{ color: 'red' }}>{message}</p>}
