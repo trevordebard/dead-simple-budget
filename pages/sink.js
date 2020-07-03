dd10f374085af0e5e21b87e07c2303ffac5ffa59
Trevor Debardeleben <trevordebardeleben@Trevors-MacBook-Pro.local>
2020-07-02 19:15:08 -0500

    Remove button definitions in file

--- pages/sink.js
+++ pages/sink.js
@@ -1,46 +1,5 @@
-import styled, { css } from 'styled-components';
-
-const Button = styled.button`
-  background-color: var(--primary);
-  color: white;
-  border-radius: 45px;
-  border: 0px;
-  padding: 5px 20px;
-  font-size: ${({ small }) => (small ? 'var(--smallFontSize)' : 'inherit')};
-  cursor: pointer;
-  &:hover {
-    box-shadow: var(--level2);
-    background-color: var(--primaryLight);
-  }
-  ${({ small }) => {
-    if (small) {
-      return css`
-        font-size: var(--smallFontSize);
-        padding: 5px 15px;
-      `;
-    }
-  }}
-`;
-
-const ActionButton = styled(Button)`
-  background: var(--action);
-  &:hover {
-    background-color: var(--actionLight);
-  }
-`;
-const DangerButton = styled(Button)`
-  background: var(--danger);
-  &:hover {
-    background-color: var(--dangerLight);
-  }
-`;
-const TransparentButton = styled(Button)`
-  background: transparent;
-  color: var(--fontColor);
-  &:hover {
-    background-color: transparent;
-  }
-`;
+import styled from 'styled-components';
+import { Button, ActionButton, DangerButton } from '../components/styled';
 
 const ButtonContainer = styled.div`
   /* display: block; */
