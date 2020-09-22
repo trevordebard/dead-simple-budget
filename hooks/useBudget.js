4caf591e5eb2eac6d658b07ee2d6d226d253f32c
Trevor Debardeleben <trevordebardeleben@Trevors-MacBook-Pro.local>
2020-09-21 22:05:47 -0400

    Update cache on addStack

--- hooks/useBudget.js
+++ hooks/useBudget.js
@@ -1,4 +1,5 @@
 import { gql, useQuery, useMutation } from '@apollo/client';
+import { GET_ME } from '../graphql/queries/GET_ME';
 
 const UPDATE_STACK = gql`
   mutation($budgetId: Int!, $label: String!, $amount: Float!) {
@@ -25,6 +26,8 @@ const ADD_STACK = gql`
     createOnestacks(data: { label: $newStackLabel, budget: { connect: { id: $budgetId } } }) {
       label
       amount
+      id
+      budgetId
     }
   }
 `;
@@ -62,11 +65,24 @@ const GET_BUDGET = gql`
 `;
 
 const useBudget = () => {
-  const { data, loading, error } = useQuery(GET_BUDGET);
+  const { data, loading, error } = useQuery(GET_BUDGET, { fetchPolicy: 'cache-only' });
   // TODO: Update stack label cache on addStack
   // For some reason GET_STACK_LABELS is not refetched on add stack, but it is on remove stack ???
   // Not looking deep into this since this will all be changed once I start working with cache more effectively
-  const [addStack] = useMutation(ADD_STACK);
+  const [addStack] = useMutation(ADD_STACK, {
+    update(cache, { data: result }) {
+      const existingBudget = cache.readQuery({
+        query: GET_ME,
+      });
+
+      const newBudget = JSON.parse(JSON.stringify(existingBudget));
+      newBudget.me.budget[0].stacks = [...newBudget.me.budget[0].stacks, result.createOnestacks];
+      cache.writeQuery({
+        query: GET_BUDGET,
+        data: { me: newBudget.me },
+      });
+    },
+  });
   const [updateStack] = useMutation(UPDATE_STACK);
   const [removeStack] = useMutation(REMOVE_STACK, { refetchQueries: ['GET_BUDGET', 'GET_STACK_LABELS'] });
   const [updateTotal] = useMutation(UPDATE_TOTAL);
