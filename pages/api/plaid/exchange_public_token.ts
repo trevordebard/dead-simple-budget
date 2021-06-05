e5cc22f0512530b22543b462e3f0e4b34b185f0a
Trevor DeBardeleben <trevordebard@gmail.com>
2021-06-05 18:40:09 -0400

    Use access tokens with Plaid interaction

--- pages/api/plaid/exchange_public_token.ts
+++ pages/api/plaid/exchange_public_token.ts
@@ -1,10 +1,11 @@
 import plaid from 'plaid';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { format, sub } from 'date-fns';
+import { PrismaClient } from '@prisma/client'
+import { getSession } from 'next-auth/client';
+const prisma = new PrismaClient({});
 
 const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
 const PLAID_SECRET = process.env.PLAID_SECRET_SANDBOX;
-const PLAID_ENV = process.env.PLAID_ENV;
 
 // Initialize the Plaid client
 export const client = new plaid.Client({
@@ -18,8 +19,13 @@ export const client = new plaid.Client({
     }
 });
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+    // TODO: redirect if user is not logged in
+    const session = await getSession({ req })
     console.log(req.body.publicToken)
     const data = await client.exchangePublicToken(req.query.publicToken as string)
+
+    // add bank account and access token to db
+    await prisma.bankAccout.create({ data: { plaidAccessToken: data.access_token, plaidItemId: data.item_id, user: { connect: { email: session.user.email } } } })
     res.status(200).json(data)
 }
 
--- pages/plaid.tsx
+++ pages/plaid.tsx
@@ -1,30 +1,43 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { usePlaidLink } from 'react-plaid-link';
+import plaid from 'plaid';
+import { PlaidLinkOptions, usePlaidLink } from 'react-plaid-link';
 import axios from 'axios';
 import { GetServerSideProps } from 'next';
+import { getSession } from 'next-auth/client';
+import { PrismaClient } from '.prisma/client';
+import { useQuery } from 'react-query';
 
-const Plaid = ({ linkToken }) => {
-  const [accessToken, setAccessToken] = useState<null | string>();
+async function fetchTransactions(params) {
+  const [, { accessTokens }] = params.queryKey;
+  const response = await axios.get('/api/plaid/get_transactions', {
+    params: { accessToken: accessTokens[0] },
+  });
+  return response;
+}
+
+const Plaid = ({ linkToken, plaidAccessTokens }) => {
+  const [accessTokens, setAccessTokens] = useState<string[]>(plaidAccessTokens);
   const [transactions, setTransactions] = useState<null | string>();
+  const { data, error: e } = useQuery(['transactions', { accessTokens }], fetchTransactions, {
+    enabled: accessTokens.length > 0,
+  });
+  useEffect(() => {
+    if (data) {
+      setTransactions(data.data);
+    }
+  }, [data]);
   const onSuccess = useCallback(async (publicToken, metadata) => {
-    // send token to server
-    console.log('success!');
-    console.log(metadata);
+    // assuming user will not call this method if they already have a bank account on file
     const data = await axios.get('/api/plaid/exchange_public_token', { params: { publicToken } });
-    setAccessToken(data.data.access_token);
-    const transactionResponse = await axios.get('/api/plaid/get_transactions', {
-      params: { accessToken: data.data.access_token },
-    });
-    console.log(transactionResponse.data);
-    setTransactions(transactionResponse.data);
+    setAccessTokens([...accessTokens, data.data.access_token]);
   }, []);
 
-  const config = {
+  const config: PlaidLinkOptions = {
     token: linkToken,
     onSuccess,
   };
 
-  const { open, ready, error } = usePlaidLink(config);
+  const { open } = usePlaidLink(config);
   return (
     <div>
       <button onClick={() => open()}>Connect a bank account</button>
@@ -35,11 +48,41 @@ const Plaid = ({ linkToken }) => {
 export default Plaid;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  let response = await axios.post('http://localhost:3000/api/plaid/create_link_token');
+  const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
+  const PLAID_SECRET = process.env.PLAID_SECRET_SANDBOX;
+
+  const plaidClient = new plaid.Client({
+    clientID: PLAID_CLIENT_ID,
+    secret: PLAID_SECRET,
+    env: plaid.environments.sandbox, // TODO: make env
+
+    options: {
+      version: '2020-09-14',
+      timeout: 30 * 60 * 1000, // 30 minutes }
+    },
+  });
+
+  const prisma = new PrismaClient();
+  const session = await getSession(context);
+
+  const accessTokenResponse = await prisma.user.findUnique({
+    where: { email: session.user.email },
+    include: { bankAccounts: true },
+  });
+  const accessTokens = accessTokenResponse.bankAccounts.map((entry) => entry.plaidAccessToken);
+
+  const linktokenResponse = await plaidClient.createLinkToken({
+    client_name: 'dead simple budget',
+    country_codes: ['US'],
+    language: 'en',
+    user: { client_user_id: '1234' },
+    products: ['auth', 'transactions'],
+  });
 
   return {
     props: {
-      linkToken: response.data.link_token,
+      linkToken: linktokenResponse.link_token,
+      plaidAccessTokens: accessTokens,
     },
   };
 };
