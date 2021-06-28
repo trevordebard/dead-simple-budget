a1e9017a3ffcba806e4614a2d48d7e3a5c3aee37
Trevor DeBardeleben <trevordebard@gmail.com>
2021-06-27 21:05:03 -0400

    Retrieve user using auth

--- lib/api-helpers/getUser.ts
+++ lib/api-helpers/getUser.ts
@@ -1,9 +1,11 @@
+import prisma from 'lib/prismaClient';
 import { NextApiRequest } from 'next';
 import { getSession } from 'next-auth/client';
 
 export async function getUser(req: NextApiRequest) {
   const session = await getSession({ req });
-  // return session.user;
-  // TODO: uncomment above line
-  return { id: 1 };
+  const user = await prisma.user.findUnique({
+    where: { email: session.user.email },
+  });
+  return user;
 }
