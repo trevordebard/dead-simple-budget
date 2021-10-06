a98b9bbad02654a200dec6a245719c4c15d017cb
Trevor DeBardeleben <trevordebard@gmail.com>
2021-10-06 17:22:47 -0400

    Working controlled dropdown

--- components/Transactions/EditTransaction.tsx
+++ components/Transactions/EditTransaction.tsx
@@ -1,12 +1,13 @@
-import { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import { useForm } from 'react-hook-form';
+import React, { useEffect, useState, forwardRef } from 'react';
+import styled, { css } from 'styled-components';
+import { Controller, useForm } from 'react-hook-form';
 import { formatDate } from '../../lib/formatDate';
-import { Button, RadioButton, RadioGroup, Input, Select } from '../Styled';
+import { Button, RadioButton, RadioGroup, Input, Select, ListRow } from '../Styled';
 import { ErrorText } from './NewTransaction';
-import { useEditTransaction, useTransaction } from 'lib/hooks';
+import { useEditTransaction, useStacks, useTransaction } from 'lib/hooks';
 import useStackLabels from 'lib/hooks/stack/useStackLabels';
 import { centsToDollars, dollarsToCents } from 'lib/money';
+import ClickAwayListener from 'components/Shared/ClickAway';
 
 const EditTransactionWrapper = styled.form`
   display: flex;
@@ -26,14 +27,25 @@ const EditTransactionWrapper = styled.form`
   }
 `;
 
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' },
+];
+
 const EditTransaction = ({ transactionId, cancelEdit }) => {
   const {
     register,
     handleSubmit,
     formState: { errors },
+    control,
+    setValue,
   } = useForm();
   const { stackLabels } = useStackLabels();
+  const { data: stacks } = useStacks();
   const { mutate: editTransaction } = useEditTransaction();
+  const [isOpen, setOpen] = useState(false);
+  const toggleDropdown = () => setOpen(!isOpen);
 
   const [selectedStack, setSelectedStack] = useState<string>();
   const [transactionType, setTransactionType] = useState<string | null>();
@@ -46,28 +58,33 @@ const EditTransaction = ({ transactionId, cancelEdit }) => {
     }
   }, [transaction, setTransactionType, setSelectedStack]);
 
-  const onSubmit = payload => {
-    const { date, stack, description } = payload;
-    let { amount } = payload;
-    amount = parseFloat(amount);
-    amount = dollarsToCents(amount);
+  useEffect(() => {
+    setValue('stack', selectedStack);
+  }, [setValue, selectedStack]);
 
-    if (transactionType === 'withdrawal') {
-      amount = -amount;
-    }
-    editTransaction(
-      {
-        transactionId,
-        transactionInput: {
-          description,
-          stack,
-          amount,
-          type: transactionType,
-          date,
-        },
-      },
-      { onSuccess: () => cancelEdit() }
-    );
+  const onSubmit = payload => {
+    console.log({ ...payload });
+    // const { date, stack, description } = payload;
+    // let { amount } = payload;
+    // amount = parseFloat(amount);
+    // amount = dollarsToCents(amount);
+
+    // if (transactionType === 'withdrawal') {
+    //   amount = -amount;
+    // }
+    // editTransaction(
+    //   {
+    //     transactionId,
+    //     transactionInput: {
+    //       description,
+    //       stack,
+    //       amount,
+    //       type: transactionType,
+    //       date,
+    //     },
+    //   },
+    //   { onSuccess: () => cancelEdit() }
+    // );
   };
   if (!transaction && !isLoadingTransaction) {
     return <h1 style={{ textAlign: 'center' }}>Not Found</h1>;
@@ -106,25 +123,35 @@ const EditTransaction = ({ transactionId, cancelEdit }) => {
       />
 
       <label htmlFor="stack">Stack {errors.stack && <ErrorText> (Required)</ErrorText>}</label>
-      <Select
+      <Controller
+        control={control}
         name="stack"
-        {...register('stack', {
-          required: true,
-          validate: value => {
-            return value !== 'Imported';
-          },
-        })}
-        value={selectedStack}
-        onChange={e => setSelectedStack(e.target.value)}
-      >
-        {transaction.stack === 'Imported' && <option disabled>Imported</option>}
-        {stackLabels &&
-          stackLabels.map(label => (
-            <option key={`${label}-${Date.now()}`} value={label}>
-              {label}
-            </option>
-          ))}
-      </Select>
+        rules={{ validate: value => value !== 'Imported' }}
+        render={({ field }) => (
+          <div>
+            <DropdownWrapper>
+              <DropdownHeader onClick={toggleDropdown} tabIndex={0}>
+                {selectedStack}
+              </DropdownHeader>
+              <DropdownBody isOpen={isOpen}>
+                {stacks.map(stack => (
+                  <div
+                    onClick={e => {
+                      field.onChange(stack.label);
+                      setSelectedStack(stack.label);
+                    }}
+                    key={stack.id}
+                  >
+                    <div>{stack.label}</div>
+                    <div>{stack.amount}</div>
+                  </div>
+                ))}
+              </DropdownBody>
+            </DropdownWrapper>
+          </div>
+        )}
+      />
+
       <label htmlFor="date">Date {errors.date && <ErrorText> (Required)</ErrorText>}</label>
       <Input
         name="date"
@@ -164,3 +191,65 @@ const EditTransaction = ({ transactionId, cancelEdit }) => {
   );
 };
 export default EditTransaction;
+
+const StackSelectorField = ({ value, onChange }) => <StackSelector seletedStack={value} setSelectedStack={onChange} />;
+
+const StackSelector = ({ seletedStack, setSelectedStack }) => {
+  const [isOpen, setOpen] = useState(false);
+  const { data: stacks } = useStacks();
+
+  const toggleDropdown = () => setOpen(!isOpen);
+
+  const handleItemClick = label => {
+    seletedStack == label ? setSelectedStack(null) : setSelectedStack(label);
+  };
+
+  if (!stacks) {
+    return null;
+  }
+
+  return (
+    <ClickAwayListener onClickAway={() => setOpen(false)}>
+      <DropdownWrapper>
+        <DropdownHeader onClick={toggleDropdown} tabIndex={0}>
+          {seletedStack}
+        </DropdownHeader>
+        <DropdownBody isOpen={isOpen}>
+          {stacks.map(stack => (
+            <ListRow selected={seletedStack === stack.id} onClick={e => handleItemClick(stack.label)} key={stack.id}>
+              <div>{stack.label}</div>
+              <div>{centsToDollars(stack.amount)}</div>
+            </ListRow>
+          ))}
+        </DropdownBody>
+      </DropdownWrapper>
+    </ClickAwayListener>
+  );
+};
+
+StackSelector.displayName = 'StackSelector';
+
+const DropdownWrapper = styled.div`
+  border-radius: 5px;
+  position: relative;
+  border: 1px solid var(--grey-800);
+`;
+const DropdownHeader = styled.div`
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.4rem;
+`;
+const DropdownBody = styled.div<{ isOpen: boolean }>`
+  padding: 5px;
+  border: 1px solid black;
+  display: ${props => (props.isOpen ? 'block' : 'none')};
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin-top: 5px;
+  background-color: white;
+`;
+
+const DropdownItemDot = styled.span``;
