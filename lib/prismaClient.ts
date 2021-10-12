d0b0f1c68665756940954ac15d7cfe0380fb1afc
Trevor DeBardeleben <trevordebard@gmail.com>
2021-10-12 18:34:02 -0400

    Upgrade (most) packages

--- lib/prismaClient.ts
+++ lib/prismaClient.ts
@@ -1,17 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
-// add prisma to the NodeJS global type
-
-interface CustomNodeJsGlobal extends NodeJS.Global {
-  prisma: PrismaClient;
+declare global {
+  var prisma: PrismaClient | undefined;
 }
-
-// Prevent multiple instances of Prisma Client in development
-
-declare const global: CustomNodeJsGlobal;
 
 const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
 export default prisma;
