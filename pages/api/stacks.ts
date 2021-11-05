22201e9af9304e011f1c677c45b44fee71d5da7d
Trevor DeBardeleben <trevordebard@gmail.com>
2021-11-05 15:06:47 -0400

    Add API route to fetch stacks by category

--- pages/api/stacks.ts
+++ pages/api/stacks.ts
@@ -1,17 +1,18 @@
-import { deleteTransactions } from 'lib/api-helpers/deleteTransactions';
 import { getUser } from 'lib/api-helpers/getUser';
 import prisma from 'lib/prismaClient';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { iCategorizedStack, iGetStacksOptions } from 'types/stack';
 
 export default async function stacksHandler(req: NextApiRequest, res: NextApiResponse) {
-  const { method } = req;
+  const { method, query } = req;
 
   const user = await getUser(req);
 
   switch (method) {
     case 'GET':
+      const options: iGetStacksOptions = query;
       // Get data from your database
-      const stacks = await getStacks(user.id);
+      const stacks = await getStacks(user.id, options);
       res.status(200).json(stacks);
       break;
     default:
@@ -20,6 +21,27 @@ export default async function stacksHandler(req: NextApiRequest, res: NextApiRes
   }
 }
 
-async function getStacks(userId: number) {
-  return await prisma.stack.findMany({ where: { userId } });
+async function getStacks(userId: number, options: iGetStacksOptions = null) {
+  if (options?.organizeBy === 'category') {
+    return getStacksByCategory(userId);
+  }
+  return await prisma.stack.findMany({ where: { userId }, include: { category: true } });
+}
+
+// TODO: optimizations
+async function getStacksByCategory(userId: number): Promise<iCategorizedStack[]> {
+  const categories = await prisma.stackCategory.findMany({ where: { userId } });
+  let categorizedStacks = await Promise.all(
+    categories.map(async cat => {
+      const stacks = await prisma.stack.findMany({ where: { userId, stackCategoryId: cat.id } });
+      stacks.sort(function (a, b) {
+        return cat.stackOrder.indexOf(a.id) - cat.stackOrder.indexOf(b.id);
+      });
+      return {
+        category: cat.category,
+        stacks: stacks,
+      };
+    })
+  );
+  return categorizedStacks;
 }
