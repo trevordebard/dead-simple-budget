0efa594b6c104fee652b4a507d178d9ef0e0ce1a
Trevor DeBardeleben <trevordebard@gmail.com>
2021-11-05 15:06:02 -0400

    Update schema to include stack category

--- types/stack.ts
+++ types/stack.ts
@@ -1,12 +1,21 @@
+import { Stack } from '.prisma/client';
+
 export interface iCreateStackInput {
   label: string;
-  category?: string;
   amount?: number;
 }
 
 export interface iUpdateStackInput {
   id: number;
   label: string;
-  category?: string;
   amount?: number;
+}
+
+export interface iGetStacksOptions {
+  organizeBy?: 'category';
+}
+
+export interface iCategorizedStack {
+  category: string;
+  stacks: Stack[];
 }
