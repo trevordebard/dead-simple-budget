4ef30e2c32c9dc46da7e9e1a0dd01ed570f44e68
Trevor DeBardeleben <trevordebard@gmail.com>
2021-11-28 13:56:42 -0500

    Update cross origin

--- pages/_document.js
+++ pages/_document.js
@@ -31,7 +31,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600&display=swap"
             rel="stylesheet"
