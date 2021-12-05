172e5c200f9540047d5e7e689b23979a422a0a03
Trevor DeBardeleben <trevordebard@gmail.com>
2021-12-05 15:29:01 -0500

    Redirect if logged in

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,3 +1,11 @@
+import { LoaderFunction, redirect } from "remix";
+import { authenticator } from "~/auth/auth.server";
+
+export let loader: LoaderFunction = async ({ request }) => {
+  let user = await authenticator.isAuthenticated(request);
+  if (user) return redirect("/budget");
+  return null;
+};
 export default function Login() {
   return (
     <form action="/auth/google" method="post">
