f231712bc5b7ed3b3661bfb00940eb7052ddf910
Trevor DeBardeleben <trevordebard@gmail.com>
2021-12-06 19:57:14 -0500

    Implement add stack category form

--- app/routes/budget.tsx
+++ app/routes/budget.tsx
@@ -42,12 +42,18 @@ export const action: ActionFunction = async ({ request }) => {
   const user = await requireAuthenticatedUser(request);
 
   const formData = await request.formData();
-  const isAddStackForm = formData.has('new-stack-label');
+  const isAddStackForm = formData.has('new-stack');
+  const isAddCategoryForm = formData.has('new-category');
 
   if (isAddStackForm) {
-    const label = String(formData.get('new-stack-label'));
+    const label = String(formData.get('new-stack'));
     const newStack = await createStack(user, { label });
     return newStack;
+  }
+  if (isAddCategoryForm) {
+    const label = String(formData.get('new-category'));
+    const newCategory = await db.stackCategory.create({ data: { label, budgetId: user.Budget.id } });
+    return newCategory;
   }
   const input = formData.forEach(async (value, key) => {
     await db.stack.update({
@@ -82,7 +88,7 @@ export default function Index() {
                           name={stack.label}
                           id={stack.id.toString()}
                           defaultValue={stack.amount}
-                          className="text-right border-0 rounded-md max-w-xs hover:bg-gray-100 py-6"
+                          className="text-right border-0 rounded-md max-w-xs hover:bg-gray-100 py-6 px-4"
                           onBlur={(e) => submit(e.currentTarget.form)}
                         />
                         <Link to={`/budget/stack/${stack.id}`} className="text-gray-600">
@@ -97,7 +103,17 @@ export default function Index() {
           </Form>
           <Form method="post" id="add-stack-form">
             <div className="flex justify-between space-x-4 items-center">
-              <input type="text" name="new-stack-label" placeholder="New Stack Name" />
+              <input type="text" name="new-stack" placeholder="New Stack Name" />
+              <input
+                type="submit"
+                defaultValue="Submit"
+                className="rounded-md cursor-pointer px-4 py-2 bg-blue-500 text-blue-100 hover:bg-blue-600"
+              />
+            </div>
+          </Form>
+          <Form method="post" id="add-category-form">
+            <div className="flex justify-between space-x-4 items-center">
+              <input type="text" name="new-category" placeholder="New Category Name" />
               <input
                 type="submit"
                 defaultValue="Submit"
