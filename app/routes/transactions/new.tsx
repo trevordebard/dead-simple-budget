0969f6ecf4aa20fbf1d1dee6feca31309b4e25ea
Trevor DeBardeleben <trevordebard@gmail.com>
2022-01-03 20:30:19 -0500

    Use Radix UI toggle group for transaction type

--- app/routes/transactions/new.tsx
+++ app/routes/transactions/new.tsx
@@ -1,4 +1,6 @@
 import { ActionFunction, Form } from 'remix';
+import * as ToggleGroup from '@radix-ui/react-toggle-group';
+import { useState } from 'react';
 import { db } from '~/utils/db.server';
 import { requireAuthenticatedUser } from '~/utils/server/index.server';
 
@@ -9,12 +11,15 @@ export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
 
   const description = String(formData.get('description'));
-  const amount = Number(formData.get('amount'));
+  let amount = Number(formData.get('amount'));
   const stack = String(formData.get('stack'));
   const type = String(formData.get('trans-type'));
 
   if (!amount || !stack || !description || !type) {
     throw Error('TODO');
+  }
+  if (type === 'withdrawal') {
+    amount *= -1;
   }
 
   const create = await db.transaction.create({
@@ -24,29 +29,64 @@ export const action: ActionFunction = async ({ request }) => {
 };
 
 export default function NewTransaction() {
+  const [transactionType, setTransactionType] = useState<string>('deposit');
   return (
     <div className="fixed top-0 bottom-0 left-0 right-0 md:relative bg-white p-5 md:p-0">
-      <Form method="post">
-        <label htmlFor="description">Description</label>
-        <input type="text" name="description" id="description-input" />
-        <label htmlFor="amount">Amount</label>
-        <input type="text" name="amount" id="amount-input" />
-        <label htmlFor="stack">Stack</label>
-        <input type="text" name="stack" id="stack-input" />
-        <div className="flex justify-between">
-          <div>
-            <input id="deposit" value="deposit" type="radio" name="trans-type" />
-            <label htmlFor="deposit">Deposit</label>
-          </div>
-          <div>
-            <input id="withdrawal" value="withdrawal" type="radio" name="trans-type" />
-            <label htmlFor="withdrawal">Withdrawal</label>
-          </div>
+      <h3 className="text-lg mb-3 divide-y-2 text-center">New Transaction</h3>
+      <Form method="post" className="space-y-4">
+        <div>
+          <label htmlFor="description">Description</label>
+          <input
+            type="text"
+            name="description"
+            id="description-input"
+            className="border border-gray-300 sm:text-sm rounded-md py-5"
+          />
+        </div>
+        <div>
+          <label htmlFor="amount">Amount</label>
+          <input
+            type="text"
+            name="amount"
+            id="amount-input"
+            className="border border-gray-300 sm:text-sm rounded-md py-5"
+          />
+        </div>
+        <div>
+          <label htmlFor="stack">Stack</label>
+          <input
+            type="text"
+            name="stack"
+            id="stack-input"
+            className="border border-gray-300 sm:text-sm rounded-md py-5"
+          />
+        </div>
+        <div>
+          <input type="hidden" name="trans-type" id="trans-type" value={transactionType} />
+          <ToggleGroup.Root
+            type="single"
+            defaultValue="deposit"
+            className="inline-flex rounded-md w-full"
+            onValueChange={(val) => setTransactionType(val)}
+          >
+            <ToggleGroup.Item
+              value="deposit"
+              className="flex items-center justify-center w-full h-9 border-gray-300 border hover:bg-gray-100 first:rounded-l-md last:rounded-r-md radix-state-on:border-transparent radix-state-on:bg-purple-900 radix-state-on:text-purple-50 focus:outline-none "
+            >
+              Deposit
+            </ToggleGroup.Item>
+            <ToggleGroup.Item
+              value="withdrawal"
+              className="flex items-center justify-center w-full h-9 border-gray-300 border hover:bg-gray-100 first:rounded-l-md last:rounded-r-md radix-state-on:border-transparent radix-state-on:bg-purple-900 radix-state-on:text-purple-50 focus:outline-none "
+            >
+              Withdrawal
+            </ToggleGroup.Item>
+          </ToggleGroup.Root>
         </div>
         <input
           type="submit"
-          className="rounded-md cursor-pointer px-4 py-2 bg-gray-700 text-gray-50 hover:bg-gray-600 font-normal"
-          value="Add"
+          className="rounded-md cursor-pointer px-4 py-2 border border-gray-700  hover:bg-gray-700 hover:text-gray-50 w-full"
+          value="Add Transaction"
         />
       </Form>
     </div>
