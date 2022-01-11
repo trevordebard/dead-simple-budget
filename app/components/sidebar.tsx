518faa6cf734c81e9a5e604793cbe691fbddf607
Trevor DeBardeleben <trevordebard@gmail.com>
2022-01-10 19:34:25 -0500

    Use clsx to style sidebar

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { useLocation, Link } from 'remix';
 
 export function Sidebar() {
@@ -7,21 +8,23 @@ export function Sidebar() {
       <nav className="flex justify-center items-center text-base space-x-5 md:flex-col md:space-y-5 md:space-x-0">
         <Link
           to="/budget"
-          className={
-            pathname.split('/')[1] === 'budget'
-              ? 'px-4 py-2 md:py-3 text-sm font-semibold bg-purple-200 rounded-lg md:w-full text-purple-800 hover:no-underline hover:text-purple-800'
-              : 'px-4 py-2 md:py-3 text-sm font-semibold md:w-full rounded-lg text-gray-900 hover:bg-gray-100 hover:no-underline hover:text-gray-800'
-          }
+          className={clsx('px-4 py-2 md:py-3 text-sm font-medium ', {
+            'bg-purple-200 rounded-lg md:w-full text-purple-800 hover:no-underline hover:text-purple-800':
+              pathname.split('/')[1] === 'budget',
+            'md:w-full rounded-lg text-gray-900 hover:bg-gray-100 hover:no-underline hover:text-gray-800':
+              pathname.split('/')[1] !== 'budget',
+          })}
         >
           Budget
         </Link>
         <Link
           to="/transactions"
-          className={
-            pathname.split('/')[1] === 'transactions'
-              ? 'px-4 py-2 md:py-3 text-sm font-semibold bg-purple-200 rounded-lg md:w-full text-purple-800 hover:no-underline hover:text-purple-800'
-              : 'px-4 py-2 md:py-3 text-sm font-semibold md:w-full rounded-lg text-gray-900 hover:bg-gray-100 hover:no-underline hover:text-gray-800'
-          }
+          className={clsx(`px-4 py-2 md:py-3 text-sm font-medium`, {
+            'bg-purple-200 rounded-lg md:w-full text-purple-800 hover:no-underline hover:text-purple-800':
+              pathname.split('/')[1] === 'transactions',
+            'md:w-full rounded-lg text-gray-900 hover:bg-gray-100 hover:no-underline hover:text-gray-800':
+              pathname.split('/')[1] !== 'transactions',
+          })}
         >
           Transactions
         </Link>
