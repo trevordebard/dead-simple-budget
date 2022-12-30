4a3b7628a4a10d3dab1f67013815c67b64b8d168
Trevor DeBardeleben <trevordebard@gmail.com>
2022-12-30 09:52:16 -0500

    Optimistic UI for new transactions

--- app/routes/transactions.tsx
+++ app/routes/transactions.tsx
@@ -1,14 +1,14 @@
-import { LoaderArgs } from '@remix-run/node';
+import { json, LoaderArgs, SerializeFrom } from '@remix-run/node';
 import { DateTime } from 'luxon';
 import { CurrencyDollarIcon } from '@heroicons/react/outline';
-import { typedjson, useTypedLoaderData } from 'remix-typedjson';
-import { Link, Outlet } from '@remix-run/react';
-import { Stack, Transaction } from '.prisma/client';
+import { Link, Outlet, useFetchers, useLoaderData } from '@remix-run/react';
 import { ContentAction, ContentLayout, ContentMain } from '~/components/layout';
 import { db } from '~/lib/db.server';
 import { centsToDollars } from '~/lib/modules/money';
 
 import { requireAuthenticatedUser } from '~/lib/modules/user';
+import { Stack, Transaction } from '@prisma/client';
+import { NewTransactionSchema, validateAction } from '~/lib/modules/validation';
 
 export async function loader({ request }: LoaderArgs) {
   const user = await requireAuthenticatedUser(request);
@@ -18,12 +18,37 @@ export async function loader({ request }: LoaderArgs) {
     throw Error('TODO');
   }
 
-  const transactions = await db.transaction.findMany({ where: { budgetId: budget.id }, include: { stack: true } });
-  return typedjson({ transactions });
+  const transactions = await db.transaction.findMany({
+    where: { budgetId: budget.id },
+    include: { stack: true },
+    orderBy: [{ date: 'desc' }, { createdAt: 'desc' }],
+  });
+  return json({ transactions });
 }
 
 export default function TransactionsPage() {
-  const { transactions } = useTypedLoaderData<typeof loader>();
+  const { transactions } = useLoaderData<typeof loader>();
+  const fetchers = useFetchers();
+
+  let optimisticTransaction;
+  for (const f of fetchers) {
+    if (f.submission && f.submission.action.startsWith(`/transactions/new`)) {
+      const { formData, errors } = validateAction({
+        schema: NewTransactionSchema,
+        formData: f.submission.formData,
+      });
+      if (!errors) {
+        optimisticTransaction = {
+          ...formData,
+          stack: {
+            label: 'Pending...',
+          },
+          date: DateTime.fromJSDate(formData.date).toFormat('yyyy-MM-dd'),
+          amount: Number(centsToDollars(formData.amount)) || 0,
+        };
+      }
+    }
+  }
 
   return (
     <ContentLayout>
@@ -44,15 +69,18 @@ export default function TransactionsPage() {
               </p>
             </div>
           ) : (
-            transactions.map((t) => (
-              <Link
-                to={`${t.id}`}
-                key={t.id}
-                className="hover:no-underline hover:text-inherit focus:outline-none focus:bg-gray-200"
-              >
-                <TransactionCard transaction={t} />
-              </Link>
-            ))
+            <>
+              {optimisticTransaction ? <TransactionCard transaction={optimisticTransaction} /> : null}
+              {transactions.map((t) => (
+                <Link
+                  to={`${t.id}`}
+                  key={t.id}
+                  className="hover:no-underline hover:text-inherit focus:outline-none focus:bg-gray-200"
+                >
+                  <TransactionCard transaction={t} />
+                </Link>
+              ))}
+            </>
           )}
         </div>
       </ContentMain>
@@ -63,10 +91,15 @@ export default function TransactionsPage() {
   );
 }
 
+// Data coming from loader will be serialized. Fields like dates will be strings
+type SerializedTransactionWithStack = SerializeFrom<
+  Transaction & {
+    stack?: Pick<Stack, 'label'> | null;
+  }
+>;
+
 type iTransactionCardProps = {
-  transaction: Transaction & {
-    stack: Stack | null;
-  };
+  transaction: Partial<SerializedTransactionWithStack>;
 };
 
 export function TransactionCard({ transaction }: iTransactionCardProps) {
@@ -79,7 +112,7 @@ export function TransactionCard({ transaction }: iTransactionCardProps) {
       </div>
       <div>
         <p className="text-right">${centsToDollars(amount)}</p>
-        <p className="text-zinc-600">{DateTime.fromJSDate(date, { zone: 'UTC' }).toFormat('yyyy-MM-dd')}</p>
+        <p className="text-zinc-600">{date && DateTime.fromISO(date).toFormat('yyyy-MM-dd')}</p>
       </div>
     </div>
   );
